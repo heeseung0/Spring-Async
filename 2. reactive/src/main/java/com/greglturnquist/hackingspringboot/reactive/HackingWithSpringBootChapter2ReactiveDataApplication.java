860dea59411ffbb47fbee1e36db72ae3851a46db
heeseung <ys2645@naver.com>
2024-06-24 16:42:22 +0900

    Ch.3 debug, log  2

--- 2. reactive/src/main/java/com/greglturnquist/hackingspringboot/reactive/HackingWithSpringBootChapter2ReactiveDataApplication.java
+++ 2. reactive/src/main/java/com/greglturnquist/hackingspringboot/reactive/HackingWithSpringBootChapter2ReactiveDataApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HackingWithSpringBootChapter2ReactiveDataApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(HackingWithSpringBootChapter2ReactiveDataApplication.class, args);
 	}
